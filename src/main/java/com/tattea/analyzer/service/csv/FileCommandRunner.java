0adf31154f53e531d1be66dfc910439ae510f9f0
stephanbaichoo <baichoostephan@gmail.com>
2022-07-10 14:30:13 +0400

    bash command runner

---
+++ src/main/java/com/tattea/analyzer/service/csv/FileCommandRunner.java
@@ -0,0 +1,28 @@
+package com.tattea.analyzer.service.csv;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import lombok.SneakyThrows;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileCommandRunner {
+
+    private static final String csvDirectory = "";
+
+    public void cutAllCSVFiles(String csvDirectory) {}
+
+    @SneakyThrows
+    private void runCommand(String cmd) {
+        Runtime r = Runtime.getRuntime();
+
+        String[] commands = { "bash", "-c", cmd };
+
+        Process p = r.exec(commands);
+        p.waitFor();
+    }
+
+    private String buildListCmd(List<String> cmds) {
+        return cmds.stream().map(cmd -> cmd.concat(" ;")).collect(Collectors.joining());
+    }
+}
